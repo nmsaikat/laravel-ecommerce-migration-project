34deae31c5c4ae1ddd13c8dd0ac12546a0426d14
nmsaikat <nmsaikat.cf@gmail.com>
2025-12-11 08:47:08 +0600

    Create users table

--- database/migrations/2025_12_10_173740_create_users.php
+++ database/migrations/2025_12_10_173740_create_users.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('email')->unique();
+            $table->string('otp');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
