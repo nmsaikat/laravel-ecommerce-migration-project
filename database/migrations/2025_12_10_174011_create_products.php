296e3743705c0616603024e062718e2a1ac18067
nmsaikat <nmsaikat.cf@gmail.com>
2025-12-12 11:49:53 +0600

    I created a products SQL table and connected it to the category and brand tables using foreign keys to maintain proper relational structure.

--- database/migrations/2025_12_10_174011_create_products.php
+++ database/migrations/2025_12_10_174011_create_products.php
@@ -13,7 +13,32 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title', 200);
+            $table->string('short_description', 500);
+            $table->string('price', 50);
+            $table->boolean('discount');
+            $table->string('discount_price', 50);
+            $table->string('image', 300);
+            $table->boolean('stock');
+            $table->float('star');
+            $table->enum('remark', ['new', 'popular', 'top', 'special', 'trending', 'regular']);
+
+            // Category Foreign Keys
+            $table->unsignedBigInteger('category_id');
+
+            // Brand Foreign Keys
+            $table->unsignedBigInteger('brand_id');
+
+            // Category Table Relation
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->restrictOnDelete()->cascadeOnUpdate();
+
+            // Brand Table Relation
+            $table->foreign('brand_id')->references('id')->on('brands')
+                ->restrictOnDelete()->cascadeOnUpdate();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
